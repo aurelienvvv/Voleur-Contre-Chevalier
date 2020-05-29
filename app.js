de450c86478d7df76db573a8da17b02d8563e789
Aurélien Verschueren <averschueren@modedemploi.fr>
2020-05-29 11:10:13 +0200

    génération des armes et des personnages

--- app.js
+++ app.js
@@ -1,45 +1,21 @@
-let weapons = [
-    {
-        name: 'Pierre',
-        damage: '1',
-        visuel: 'img/pierre.jpg'
-    },
-    {
-        name: 'Couteau',
-        damage: '3',
-        visuel: 'img/pierre.jpg'
-    },
-    {
-        name: 'Sabre',
-        damage: '6',
-        visuel: 'img/pierre.jpg'
-    },
-    {
-        name: 'Sabre Laser',
-        damage: '10',
-        visuel: 'img/sabre-laser.jpg'
-    }
-];
-
-
-function generateRandom(array) {
-    return Math.floor(Math.random() * array.length);
+// Séléction au hasard dans un tableau
+function selectRandom(array) {
+    return array[Math.floor(Math.random() * array.length)];
 }
 
-
+// Création d'une case avec une état
 function generateCell() {
     let stateOfCell = ['vide', 'innacessible', 'vide', 'vide', 'vide'];
-    let state = stateOfCell[generateRandom(stateOfCell)];
+    let state = selectRandom(stateOfCell);
 
     return cell = {
         state: state,
         x: 0,
-        y: 0,
-        weapon: ''
+        y: 0
     };
 };
 
-
+// Génération de 100 cases
 function generateAllCells() {
     let arrCells = [];
 
@@ -50,15 +26,7 @@ function generateAllCells() {
     return arrCells
 };
 
-
-function displayCells(arrOfCells) {
-    for (let arrCell of arrOfCells) {
-        let containerCells = document.querySelector('.cells-container ul');
-        containerCells.insertAdjacentHTML('beforeend', `<div class="cell ${arrCell.state}" x="${arrCell.x}" y="${arrCell.y}"></div>`);
-    };
-};
-
-
+// Ajout des coordonnées aux cases
 function addCoordonates(arrCells) {
     let cellsWithCoordonates = [];
     let x = -1;
@@ -81,12 +49,97 @@ function addCoordonates(arrCells) {
     return cellsWithCoordonates;
 };
 
+// Ajout des armes aux cases
+function addWeapons(cells) {
+    const weapons = [
+        {
+            name: 'Poing Américain',
+            damage: '1',
+            visuel: 'img/poing.png'
+        },
+        {
+            name: 'Baton',
+            damage: '3',
+            visuel: 'img/baton.png'
+        },
+        {
+            name: 'Lance',
+            damage: '6',
+            visuel: 'img/lance.png'
+        },
+        {
+            name: 'Epee',
+            damage: '10',
+            visuel: 'img/epee.png'
+        }
+    ];
 
-function launchCells() {
+    // retourne tableau des cases vides
+    let emptyCells = cells.filter(cell => cell.state === 'vide');
+
+    // ajoute les armes aux cases vides au hasard
+    weapons.map(weapon => selectRandom(emptyCells).weapon = weapon);
+
+    // retire le statut 'vide' aux cases avec une arme
+    cells.filter(cell => cell.weapon).map(cell => cell.state = "");
+
+    return cells
+}
+
+function addPlayers(cells) {
+    const players = [
+        {
+            name: 'Player 1',
+            life: 100,
+            visuel: 'img/player-1.png'
+        },
+        {
+            name: 'Player 2',
+            life: 100,
+            visuel: 'img/player-2.png'
+        }
+    ]
+
+    // retourne tableau des cases vides
+    let emptyCells = cells.filter(cell => cell.state === 'vide');
+
+    players.map(player => selectRandom(emptyCells).player = player);
+
+    return cells
+}
+
+// Affichage des cases dans le DOM
+function displayCells(arrOfCells) {
+    let containerCells = document.querySelector('.cells-container ul');
+
+    for (let arrCell of arrOfCells) {
+
+        if (arrCell.weapon) {
+            containerCells.insertAdjacentHTML('beforeend', `<li class="cell weapon ${arrCell.state}" data-x="${arrCell.x}" data-y="${arrCell.y}">
+            <img src=${arrCell.weapon.visuel} alt="${arrCell.weapon.name}">
+            </li>`);
+        } else if (arrCell.player) {
+            containerCells.insertAdjacentHTML('beforeend', `<li class="cell ${arrCell.state}" data-x="${arrCell.x}" data-y="${arrCell.y}">
+            <img src=${arrCell.player.visuel} alt="${arrCell.player.name}">
+            </li>`);
+        } else {
+            containerCells.insertAdjacentHTML('beforeend', `<li class="cell ${arrCell.state}" data-x="${arrCell.x}" data-y="${arrCell.y}"></li>`);
+        }
+
+    };
+};
+
+// Création du plateau de jeu
+function createGame() {
     let arrCells = generateAllCells();
     let cellsWithXY = addCoordonates(arrCells);
-    displayCells(cellsWithXY);
+    let cellsWithWeapons = addWeapons(cellsWithXY);
+    let cellsWithPlayers = addPlayers(cellsWithWeapons);
+
+    console.log(cellsWithPlayers);
+
+    displayCells(cellsWithWeapons);
 };
 
 
-launchCells();
+createGame();
